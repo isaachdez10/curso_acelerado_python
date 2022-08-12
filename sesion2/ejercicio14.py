dd53a33d4a68401c3daebb758fe60427038d94ad
Replit user <>
2022-08-12 10:02:48 +0000

    ejerccicio de la segunda sesion

---
+++ sesion2/ejercicio14.py
@@ -0,0 +1,10 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 04-08-2022
+File: sesion2/ejercicio14.py
+Autor: ..............
+Action: 
+'''
+palabra = (input("Inrese una palabra: "))
+for f in range(10):
+  print(palabra)
---
+++ sesion2/ejercicio17.py
@@ -0,0 +1,11 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 04-08-2022
+File: sesion2/ejercicio13.py
+Autor: ..............
+Action: imprime un triangulo
+'''
+n = int(input("Introduce tabla: "))
+for i in range(10):
+  r=n*(i+1)
+  print(r)
